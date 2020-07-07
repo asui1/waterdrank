e327b33dc4fcd1f1946a3c54a58e2527bb251e61
asui1 <whwkd122@kaist.ac.kr>
2020-07-07 20:02:15 +0900

    initial state

---
+++ app/src/main/java/asui/water/MainActivity.kt
@@ -0,0 +1,59 @@
+package asui.water
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+//    var total_water = 2000
+//    var water1 = 30
+//    var water2 = 200
+//    var cur_depth = 0
+//    var water3 = 500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var waters = 0
+
+//        water_amount_button1.setOnClickListener{
+//            waters += water1
+//            cur_depth = water1/total_water * 650
+//            if(imageView2.layoutParams.height < cur_depth){
+//               imageView2.layoutParams.height = 0
+//            }
+//            else{
+//                imageView2.layoutParams.height -= cur_depth
+//            }
+//
+//        }
+//        water_amount_button2.setOnClickListener{
+//            waters += water2
+//            cur_depth = water2/total_water * 650
+//            if(imageView2.layoutParams.height < cur_depth){
+//                imageView2.layoutParams.height = 0
+//            }
+//            else{
+//                imageView2.layoutParams.height -= cur_depth
+//            }
+
+//        }
+//        water_amount_button3.setOnClickListener{
+//            waters += water3
+//            cur_depth = water3/total_water * 650
+//            if(imageView2.layoutParams.height < cur_depth){
+//                imageView2.layoutParams.height = 0
+//            }
+//            else{
+//                imageView2.layoutParams.height -= cur_depth
+//            }
+
+//        }
+
+    }
+}
