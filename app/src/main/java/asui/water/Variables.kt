6fe0c3eb8acb853b576c983abd501391d079a82d
asui1 <whwkd122@kaist.ac.kr>
2020-07-20 19:41:51 +0900

    Revert "Revert "ready for change values""
    This reverts commit d498c5d2

--- app/src/main/java/asui/water/Variables.kt
+++ app/src/main/java/asui/water/Variables.kt
@@ -1,6 +1,10 @@
 package asui.water
 
 import android.app.Application
+import android.content.Context
+import java.io.File
+import java.io.FileOutputStream
+import java.security.AccessController.getContext
 
 class Variables: Application() {
     companion object {
@@ -11,6 +15,7 @@ class Variables: Application() {
         var water3 = 500.0
         var waters = 0.0
         var adds = mutableListOf(0.0)
+        var updateVars = false
     }
 
 }
